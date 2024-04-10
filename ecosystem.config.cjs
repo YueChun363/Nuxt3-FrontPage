38274cae50c578d58319b049fefad59a47bd6d37
yc <1013582515@qq.com>
2024-04-10 09:32:44 +0800

    chore: pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
             name: 'NuxtAppName',
             port: '3001',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: '1',
             script: './.output/server/index.mjs',
         }
     ]
