abea06e17d55b56be36b3bcd0df1901f29480be3
yc <1013582515@qq.com>
2024-04-19 11:09:06 +0800

    feat：header中间居中、drawer、锚点

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -1,20 +1,22 @@
 <script setup lang='ts'>
 import avatar from '~/assets/imgs/1.png'
 
-const data = reactive({})
-onMounted(() => {
-
-})
+let drawer = ref(false)
+const handleAnchorClick = () => {
+    setTimeout(() => {
+        drawer.value = false
+    }, 50);
+}
 </script>
 
 <template>
     <div class="sticky top-0 bg-white">
         <div class="flex items-center justify-between px-2 py-2 max-w-7xl mx-auto md:px-6">
-            <p class="flex gap-2">
+            <p class="flex gap-2 md:flex-1">
                 <strong class="text-xl text-[#18181B]">YC's Blog</strong>
                 <el-tag type="primary" color="#eef2ff">Landing</el-tag>
             </p>
-            <div class="border-2 rounded-full px-6 pt-1 hidden md:block">
+            <div v-if="!drawer" class="border-2 rounded-full px-6 pt-1 hidden md:block">
                 <el-anchor :offset="70" direction="horizontal">
                     <el-anchor-link href="#Features">
                         Features
@@ -31,9 +33,29 @@ onMounted(() => {
                 </el-anchor>
 
             </div>
-            <div class="cursor-pointer">
+            <div v-else class="px-6 pt-1 md:hidden">
+                <client-only>
+                    <el-drawer v-model="drawer" size="100%" direction="rtl">
+                        <el-anchor :offset="70" @click="handleAnchorClick">
+                            <el-anchor-link href="#Features">
+                                Features
+                            </el-anchor-link>
+                            <el-anchor-link href="#Pricing">
+                                Pricing
+                            </el-anchor-link>
+                            <el-anchor-link href="#Testimonials">
+                                Testimonials
+                            </el-anchor-link>
+                            <el-anchor-link href="#FAQ">
+                                FAQ
+                            </el-anchor-link>
+                        </el-anchor>
+                    </el-drawer>
+                </client-only>
+            </div>
+            <div class="flex justify-end cursor-pointer md:flex-1 ">
                 <div class="inline-block text-2xl  md:hidden">
-                    <el-icon>
+                    <el-icon @click="drawer = !drawer">
                         <el-icon-operation />
                     </el-icon>
                 </div>
